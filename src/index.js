dba84b2ef45342937c8fbb85db1e2a8d3638c52a
andeylazeev <lazeev1998@yandex.ru>
2022-01-27 12:05:06 +0700

    added collision checking

--- src/index.js
+++ src/index.js
@@ -10,43 +10,53 @@ const app = new PIXI.Application({
 })
 drawBackgroundRectangles(app)
 
-function checkForCollision(obj1, obj2){
-  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy
-  obj1.centerX = obj1.x + obj1.width / 2
-  obj1.centerY = obj1.y + obj1.height / 2
-  obj2.centerX = obj2.x + obj2.width / 2
-  obj2.centerY = obj2.y + obj2.height / 2
+// function checkForCollision(obj1, obj2){
+//   let hit, combinedHalfWidths, combinedHalfHeights, vx, vy
+//   obj1.centerX = obj1.x + obj1.width / 2
+//   obj1.centerY = obj1.y + obj1.height / 2
+//   obj2.centerX = obj2.x + obj2.width / 2
+//   obj2.centerY = obj2.y + obj2.height / 2
 
-  obj1.halfWidth = obj1.width / 2
-  obj1.halfHeight = obj1.height / 2
+//   obj1.halfWidth = obj1.width / 2
+//   obj1.halfHeight = obj1.height / 2
 
-  obj2.halfWidth = obj2.width / 2
-  obj2.halfHeight = obj2.height / 2
+//   obj2.halfWidth = obj2.width / 2
+//   obj2.halfHeight = obj2.height / 2
 
-  vx = obj1.centerX - obj2.centerX
-  vy = obj1.centerY - obj2.centerY
+//   vx = obj1.centerX - obj2.centerX
+//   vy = obj1.centerY - obj2.centerY
 
-  combinedHalfWidths = obj1.halfWidth + obj2.halfWidth 
-  combinedHalfHeights = obj1.halfHeight + obj2.halfHeight
+//   combinedHalfWidths = obj1.halfWidth + obj2.halfWidth 
+//   combinedHalfHeights = obj1.halfHeight + obj2.halfHeight
 
-  if (Math.abs(vx) < combinedHalfWidths) {
-    if (Math.abs(vy) < combinedHalfHeights) {
-      hit = true
-    } else hit = false
-  }
-  else {
-    hit = false
-  }
+//   if (Math.abs(vx) < combinedHalfWidths) {
+//     if (Math.abs(vy) < combinedHalfHeights) {
+//       hit = true
+//     } else hit = false
+//   }
+//   else {
+//     hit = false
+//   }
 
-  return hit
+//   return hit
+// }
+
+function checkForCollision(a,b, offsetY) {
+  let aBox = a.getBounds()
+  let bBox = b.getBounds()
+
+  return aBox.x + aBox.width > bBox.x &&
+         aBox.x < bBox.x + bBox.width &&
+         aBox.y-offsetY + aBox.height - offsetY > bBox.y &&
+         aBox.y < bBox.y + bBox.height
 }
 
 const ticker = new PIXI.Ticker()
-const hero = new Car(app, 40, BASE_HEIGHT - 240, false)
+const hero = new Car(app, 42, BASE_HEIGHT - 240, false)
 let enemies = []
 ticker.add((delta) => {
   if (!enemies.length) {
-    const enemy = new Car(app, Math.random() > 0.5 ? 40 : BASE_WIDTH - 300, -160, true)
+    const enemy = new Car(app, Math.random() > 0.5 ? 42 : BASE_WIDTH - 300, -160, true)
     enemies.push(enemy)
   } 
   enemies = enemies.filter(enemy => {
@@ -58,14 +68,14 @@ ticker.add((delta) => {
     return true
   })
   
-  if (enemies[enemies.length - 1].root.y - 200 >= 0) {
-    const enemy = new Car(app, Math.random() > 0.5 ? 40 : BASE_WIDTH - 300, -160, true)
+  if (enemies[enemies.length - 1].root.y - 220 >= 0) {
+    const enemy = new Car(app, Math.random() > 0.5 ? 42 : BASE_WIDTH - 300, -160, true)
     enemies.push(enemy)
     return true
   }
 
   enemies.some(enemy => {
-    if (checkForCollision(enemy.root, hero.root)) {
+    if (checkForCollision(enemy.root, hero.root, 20)) {
       console.log('lose');
       return true
     }
--- src/utils/car.js
+++ src/utils/car.js
@@ -11,21 +11,20 @@ export class Car {
     for (let i = 0; i < CAR_FIGURE.length; i++) {
       for (let j = 0; j <= CAR_FIGURE[i].length; j++) {
         if (CAR_FIGURE[i][j] === 1) {
-          this.root.lineStyle(6, 0xb2beb2, 1)
           this.root.beginFill(0x000000)
-          this.root.drawRect(this.root.x + j * 40, i * 40, 40, 40)
+          this.root.drawRect(this.root.x + j * 39, i * 34, 34, 34)
           this.root.endFill()
-          // this.root.lineStyle(0)
-          // this.root.beginFill(0xb2beb2)
-          // this.root.drawRect(this.root.x + j * 40 + 7, i * 40 + 7, 26, 26)
-          // this.root.endFill()
-          // this.root.beginFill(0x000000)
-          // this.root.lineStyle(1, 0xb2beb2, 1)
-          // for (let x = 0; x < 21; x += 7) {
-          //   for (let y = 0; y < 21; y += 7) {
-          //     this.root.drawRect(this.root.x + j * 40 + (10 + x), i * 40 + (9 + y), 7, 7)
-          //   }
-          // }
+          this.root.lineStyle(0)
+          this.root.beginFill(0xb2beb2)
+          this.root.drawRect(this.root.x + j * 39 + 4, i * 34 + 4, 26, 26)
+          this.root.endFill()
+          this.root.beginFill(0x000000)
+          this.root.lineStyle(1, 0xb2beb2, 1)
+          for (let x = 0; x < 21; x += 7) {
+            for (let y = 0; y < 21; y += 7) {
+              this.root.drawRect(this.root.x + j * 39 + (7 + x), i * 34 + (6 + y), 7, 7)
+            }
+          }
           this.root.endFill()
         }
       }
@@ -38,7 +37,7 @@ export class Car {
   }
 
   update(dt) {
-    const SPEED = 2 * dt
+    const SPEED = 5 * dt
     this.root.y += SPEED
   }
 
